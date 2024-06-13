c82815b3306c44a85a3b97396edae91ac7dcf814
Remy Duivesteijn <remyd03@icloud.com>
2024-06-13 21:00:39 +0200

    v6.6 | theme color bugs ✅

--- public/js/main.js
+++ public/js/main.js
@@ -1,38 +1,43 @@
 // Meta tag color change
-
 function throttle(func, timeFrame) {
     var lastTime = 0;
     return function(...args) {
-      var now = new Date().getTime();
-      if (now - lastTime >= timeFrame) {
-        func(...args);
-        lastTime = now;
-      }
+        var now = new Date().getTime();
+        if (now - lastTime >= timeFrame) {
+            func(...args);
+            lastTime = now;
+        }
     };
-  }
-  const ogColor = document.querySelector('meta[name="theme-color"]')?.getAttribute('content');
-  
-  const handleScroll = throttle(() => {
+}
+
+const ogColor = document.querySelector('meta[name="theme-color"]')?.getAttribute('content');
+
+const handleScroll = throttle(() => {
     const targets = document.querySelectorAll('[data-scroll-theme]')
     const isTop = Array.from(targets).map((target) => {
-      const rect = target.getBoundingClientRect();
-      if (rect.y > 1) {
-        return null;
-      }
-      return { target, rect }
+        const rect = target.getBoundingClientRect();
+        if (rect.y > 1) {
+            return null;
+        }
+        return {
+            target,
+            rect
+        }
     }).filter(Boolean).sort((a, b) => b.rect.y - a.rect.y)[0]
     if (isTop) {
-  
-      const color = window.getComputedStyle(isTop.target).getPropertyValue('background-color')
-      if (color) {
-        document.querySelector('meta[name="theme-color"]')?.setAttribute('content', color);
-      }
+
+        const color = window.getComputedStyle(isTop.target).getPropertyValue('background-color')
+        if (color) {
+            document.querySelector('meta[name="theme-color"]')?.setAttribute('content', color);
+        }
     } else if (ogColor) {
-      document.querySelector('meta[name="theme-color"]')?.setAttribute('content', ogColor);
+        document.querySelector('meta[name="theme-color"]')?.setAttribute('content', ogColor);
     }
-  }, 100)
-  
-  document.addEventListener('scroll', handleScroll, { passive: true })
+}, 100)
+
+document.addEventListener('scroll', handleScroll, {
+    passive: true
+})
 
 // Introduction 1 read more button
 document.querySelector('.read-more-container').addEventListener('click', event => {
@@ -47,4 +52,3 @@ document.querySelector('.read-more-container').addEventListener('click', event =
 
     target.textContent = target.textContent === 'Read more' ? 'Read less' : 'Read more';
 });
-
